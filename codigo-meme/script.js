f1340233d94af65aea7d8acd601435a768c2d4f5
Raavë L. Aires <raave-git@proton.me>
2024-02-03 00:23:02 -0300

    código js refinado

--- codigo-meme/script.js
+++ codigo-meme/script.js
@@ -1,80 +1,64 @@
-//importação do audio do click:
+//importação dos audios:
 let pornoGay = new Audio('./assets/click.mp3');
 let cebolinha = new Audio('./assets/cebolinha.mp3')
+
+//variaveis globais
+let corpo = document.getElementsByClassName('corpo')[0];
+let titulo = document.getElementById('titulo');
+let sub = document.getElementById('subtitulo');
+let h2 = document.getElementById('pergunta');
+let divAtual = document.getElementById('botoes');
 
 //função de click sim
 let respostaSim = document.getElementById('sim').addEventListener('click',()=>{
     pornoGay.play();
 
-    let corpo = document.getElementsByClassName('corpo')[0];
-        corpo.style.backgroundColor = '#ff1fc7';
+    corpo.style.backgroundColor = '#ff1fc7';
 
-        var novaDiv = document.createElement('div');
-        var texto =  document.createTextNode('');
+        let novaDiv = document.createElement('div');
+        let texto =  document.createTextNode('');
             novaDiv.appendChild(texto);
     
-        let divAtual = document.getElementById('botoes');
             document.body.insertBefore(novaDiv,divAtual);
-
                 novaDiv.innerHTML = '<a href="https://bit.ly/rickroll-010">Agora, clique aqui</a>';
 
-        let titulo = document.getElementById('titulo');
-            titulo.innerHTML = 'Arrasou, bicha!'
+                titulo.innerHTML = 'Arrasou, bicha!'
 
-        let sub = document.getElementById('subtitulo');
-            sub.remove();
-        
-        let h2 = document.getElementById('pergunta');
-            h2.remove();
-
-            divAtual.remove();
-
-        pornoGay.play();
+                sub.remove();
+                h2.remove();
+                divAtual.remove();
 })
 
 //função de click não
 let respostaNao = document.getElementById('nao').addEventListener('click',()=>{
+    titulo.innerHTML = 'Bem-vinda à web, Fª.D.P!';
+        h2.innerHTML = 'Então, você é mulher mesmo, né?';
 
-    let titulo = document.getElementById('titulo');
-        titulo.innerHTML = 'Bem-vinda à web, Fª.D.P!';
+        let novosBotoes = document.createElement('div');
+        let botoes = document.createTextNode('');
+            novosBotoes.appendChild(botoes);
 
-    let cabecalho = document.getElementById('pergunta');
-        cabecalho.innerHTML = 'Então, você é mulher mesmo, né?';
+            document.body.insertBefore(novosBotoes,divAtual);
+                novosBotoes.innerHTML = '<button id="sim_m">Sim.</button><button id="nao_m">Não.</button>'
+                
+                divAtual.remove();
 
-    let novosBotoes = document.createElement('div');
-    let botoes = document.createTextNode('');
-        novosBotoes.appendChild(botoes);
-
-    let divAtual = document.getElementById('botoes');
-        document.body.insertBefore(novosBotoes,divAtual);
-            divAtual.remove();
-
-            novosBotoes.innerHTML = '<button id="sim_m">Sim.</button><button id="nao_m">Não.</button>'
-
-            //as arrow fuction dentro da arrow function que substituem os botoes originais por novos
+            //as arrow fuction dentro da arrow function que substituem os botões originais pelos novos
             let mulherSim = document.getElementById('sim_m').addEventListener('click',()=>{
                 titulo.innerHTML = 'Você é uma: ';
-
-                let sub = document.getElementById('subtitulo');
-                    sub.remove();
-            
-                let h2 = document.getElementById('pergunta');
-                    h2.remove();
-
                     novosBotoes.innerHTML = '<a href="https://bit.ly/rickroll-009">Agora, clique aqui</a>';
+                    
+                    sub.remove();        
+                    h2.remove();
 
                 cebolinha.play();
             });
 
             let mulherNao = document.getElementById('nao_m').addEventListener('click',()=>{
                 titulo.innerHTML = 'Pois é um viado! E ainda por cima, é um mentiroso!';
-
-                let sub = document.getElementById('subtitulo');
-                    sub.remove();
-            
-                let h2 = document.getElementById('pergunta');
-                    h2.remove();
-
                     novosBotoes.innerHTML = '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Agora, clique aqui</a>';
+
+                    sub.remove();
+                    h2.remove();
             });
 })
