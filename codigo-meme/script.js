3a785ecb57e38ff4f63f06eab6432ca94b9b8192
Raavë L. Aires <raave-git@proton.me>
2024-02-03 12:48:01 -0300

    mais comentarios

--- codigo-meme/script.js
+++ codigo-meme/script.js
@@ -17,48 +17,49 @@ let respostaSim = document.getElementById('sim').addEventListener('click',()=>{
 
         let novaDiv = document.createElement('div');
         let texto =  document.createTextNode('');
-            novaDiv.appendChild(texto);
+                novaDiv.appendChild(texto);
     
-            document.body.insertBefore(novaDiv,divAtual);
-                novaDiv.innerHTML = '<a href="https://bit.ly/rickroll-010">Agora, clique aqui</a>';
+                document.body.insertBefore(novaDiv,divAtual);
+                    novaDiv.innerHTML = '<a href="https://bit.ly/rickroll-010">Agora, clique aqui</a>';
 
-                titulo.innerHTML = 'Arrasou, bicha!'
+                    titulo.innerHTML = 'Arrasou, bicha!'
 
-                subtitulo.remove();
-                pergunta.remove();
-                divAtual.remove();
+                    subtitulo.remove();
+                    pergunta.remove();
+                    divAtual.remove();
 })
 
-//função de click não
+//função de click não:
 let respostaNao = document.getElementById('nao').addEventListener('click',()=>{
     titulo.innerHTML = 'Bem-vinda à web, Fª.D.P!';
     pergunta.innerHTML = 'Então, você é mulher mesmo, né?';
 
+        //cria uma nova div contendo novos botões:
         let novosBotoes = document.createElement('div');
         let botoes = document.createTextNode('');
-            novosBotoes.appendChild(botoes);
+                novosBotoes.appendChild(botoes);
 
-            document.body.insertBefore(novosBotoes,divAtual);
-                novosBotoes.innerHTML = '<button id="sim_m">Sim.</button><button id="nao_m">Não.</button>'
-                
-                divAtual.remove();
-
-            //as arrow fuction dentro da arrow function que substituem os botões originais pelos novos
-            let mulherSim = document.getElementById('sim_m').addEventListener('click',()=>{
-                titulo.innerHTML = 'Você é uma: ';
-                    novosBotoes.innerHTML = '<a href="https://bit.ly/rickroll-009">Agora, clique aqui</a>';
+                document.body.insertBefore(novosBotoes,divAtual);
+                    novosBotoes.innerHTML = '<button id="sim_m">Sim.</button><button id="nao_m">Não.</button>'
                     
-                    subtitulo.remove();        
-                    pergunta.remove();
+                    divAtual.remove();
 
-                cebolinha.play();
-            });
+                //funções para os novos botões:
+                let mulherSim = document.getElementById('sim_m').addEventListener('click',()=>{
+                    titulo.innerHTML = 'Você é uma: ';
+                        novosBotoes.innerHTML = '<a href="https://bit.ly/rickroll-009">Agora, clique aqui</a>';
+                        
+                        subtitulo.remove();        
+                        pergunta.remove();
 
-            let mulherNao = document.getElementById('nao_m').addEventListener('click',()=>{
-                titulo.innerHTML = 'Pois é um viado! E ainda por cima, é um mentiroso!';
-                    novosBotoes.innerHTML = '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Agora, clique aqui</a>';
+                    cebolinha.play();
+                }); //fim da função de click mulher sim
 
-                    subtitulo.remove();
-                    pergunta.remove();
-            });
-})
+                let mulherNao = document.getElementById('nao_m').addEventListener('click',()=>{
+                    titulo.innerHTML = 'Pois é um viado! E ainda por cima, é um mentiroso!';
+                        novosBotoes.innerHTML = '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Agora, clique aqui</a>';
+
+                        subtitulo.remove();
+                        pergunta.remove();
+                });//fim da função de click mulher não
+})//função de click não
